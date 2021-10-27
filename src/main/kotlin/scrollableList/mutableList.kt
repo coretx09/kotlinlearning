6932d8875f5ecd8091c72ca0793d0432d10342b2
coretx09 <ngampiosauvet@gmail.com>
2021-10-27 13:21:03 +0300

    parcourer (loop) list start study for dev app part2

--- src/main/kotlin/scrollableList/mutableList.kt
+++ src/main/kotlin/scrollableList/mutableList.kt
@@ -1,2 +1,39 @@
 package scrollableList
 
+fun main() {
+
+    // specification type
+    val entrees = mutableListOf<String>()
+    println(entrees)
+    // ou encore
+    val entrees2: MutableList<String> = mutableListOf()
+    println(entrees2)
+
+    // Add individual items using add()
+    println("Add noodles: ${entrees.add("noodles")}")
+    println("Entrees: $entrees")
+    println("Add spaghetti: ${entrees.add("spaghetti")}")
+    println("Entrees: $entrees")
+
+    // Add a list of items using addAll()
+    val moreItems = listOf("ravioli", "lasagna", "fettuccine")
+    println("Add list: ${entrees.addAll(moreItems)}")
+    println("Entrees: $entrees")
+
+    // Remove an item using remove()
+    println("Remove spaghetti: ${entrees.remove("spaghetti")}")
+    println("Entrees: $entrees")
+    println("Remove item that doesn't exist: ${entrees.remove("rice")}")
+    println("Entrees: $entrees")
+
+    // Remove an item using removeAt() with an index
+    println("Remove first element: ${entrees.removeAt(0)}")
+    println("Entrees: $entrees")
+
+    // Clear out the list
+    entrees.clear()
+    println("Entrees: $entrees")
+
+    // Check if the list is empty
+    println("Empty? ${entrees.isEmpty()}")
+}
